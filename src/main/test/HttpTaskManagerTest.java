b035084dcb1da7522ded46a0ce8493696748885d
Ustinov.vyacheslav <vustinov@fciit.ru>
2023-05-18 10:15:19 +0300

    Временный коммит.

--- src/main/test/HttpTaskManagerTest.java
+++ src/main/test/HttpTaskManagerTest.java
@@ -5,7 +5,9 @@ import main.http.KVServer;
 import main.manager.HistoryManager;
 import main.manager.Managers;
 import main.manager.TaskManager;
+import main.manager.TaskTypes;
 import main.task.Epic;
+import main.task.StatusEnum;
 import main.task.Subtask;
 import main.task.Task;
 import org.junit.jupiter.api.AfterEach;
@@ -18,7 +20,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HTTPTaskManagerTest<T extends TaskManagerTest<HttpTaskManager>> {
+class HttpTaskManagerTest<T extends TaskManagerTest<HttpTaskManager>> {
     private KVServer server;
     private TaskManager manager;
 
@@ -41,41 +43,41 @@ class HTTPTaskManagerTest<T extends TaskManagerTest<HttpTaskManager>> {
 
     @Test
     public void shouldLoadTasks() {
-        Task task1 = new Task("description1", "name1", Status.NEW, Instant.now(), 1);
-        Task task2 = new Task("description2", "name2", Status.NEW, Instant.now(), 2);
-        manager.createTask(task1);
-        manager.createTask(task2);
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task2.getId());
+        Task task1 = new Task("description1", "name1", StatusEnum.NEW, Instant.now(), 1);
+        Task task2 = new Task("description2", "name2", StatusEnum.NEW, Instant.now(), 2);
+        manager.addTask(task1);
+        manager.addTask(task2);
+        manager.getTaskByID(task1.getId());
+        manager.getTaskByID(task2.getId());
         List<Task> list = manager.getHistory();
-        assertEquals(manager.getAllTasks(), list);
+        assertEquals(manager.getTaskList(), list);
     }
 
     @Test
     public void shouldLoadEpics() {
-        Epic epic1 = new Epic("description1", "name1", Status.NEW, Instant.now(), 3);
-        Epic epic2 = new Epic("description2", "name2", Status.NEW, Instant.now(), 4);
-        manager.createEpic(epic1);
-        manager.createEpic(epic2);
-        manager.getEpicById(epic1.getId());
-        manager.getEpicById(epic2.getId());
+        Epic epic1 = new Epic("description1", "name1", StatusEnum.NEW, Instant.now(), 3, TaskTypes.EPIC);
+        Epic epic2 = new Epic("description2", "name2", StatusEnum.NEW, Instant.now(), 4, TaskTypes.EPIC);
+        manager.addEpic(epic1);
+        manager.addEpic(epic2);
+        manager.getEpicByID(epic1.getId());
+        manager.getEpicByID(epic2.getId());
         List<Task> list = manager.getHistory();
-        assertEquals(manager.getAllEpics(), list);
+        assertEquals(manager.getEpicList(), list);
     }
 
     @Test
     public void shouldLoadSubtasks() {
-        Epic epic1 = new Epic("description1", "name1", Status.NEW, Instant.now(), 5);
-        Subtask subtask1 = new Subtask("description1", "name1", Status.NEW, epic1.getId()
-                , Instant.now(), 6);
-        Subtask subtask2 = new Subtask("description2", "name2", Status.NEW, epic1.getId(),
-                Instant.now(), 7);
-        manager.createSubtask(subtask1);
-        manager.createSubtask(subtask2);
-        manager.getSubtaskById(subtask1.getId());
-        manager.getSubtaskById(subtask2.getId());
+        Epic epic1 = new Epic("description1", "name1", StatusEnum.NEW, Instant.now(), 5);
+        Subtask subtask1 = new Subtask("description1", "name1", StatusEnum.NEW, epic1.getId()
+                , Instant.now(), 6, TaskTypes.SUBTASK);
+        Subtask subtask2 = new Subtask("description2", "name2", StatusEnum.NEW, epic1.getId(),
+                Instant.now(), 7, TaskTypes.SUBTASK);
+        manager.addSubtask(subtask1);
+        manager.addSubtask(subtask2);
+        manager.getSubTaskByID(subtask1.getId());
+        manager.getSubTaskByID(subtask2.getId());
         List<Task> list = manager.getHistory();
-        assertEquals(manager.getAllSubtasks(), list);
+        assertEquals(manager.getSubtaskList(), list);
     }
 
 }
